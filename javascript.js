dd9f4a56b6e26754e1baabed7dda0bf8302ced1b
Riya Sharma <83356926+theriyasharma24@users.noreply.github.com>
2021-12-26 20:45:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function openlogin() 
+{
+    document.getElementById("loginpage").style.display = "initial";
+}
+    
+function closelogin() 
+{
+    document.getElementById("loginpage").style.display = "none";
+}
